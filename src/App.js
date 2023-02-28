1c5fad1f4da8c05c749d2a2f187437eca6effff7
Jake Connors <jacob.connors.3@spaceforce.mil>
2023-02-28 10:59:16 -1000

    Update Hello world

--- src/App.js
+++ src/App.js
@@ -15,8 +15,9 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Hello, world!!
+          Learn React
         </a>
+        Hello, world!
       </header>
     </div>
   );
